c3f8ef758ee6a54e92114967269661bf884c73b5
henry199101 <331243543@qq.com>
2017-12-02 21:43:31 +0800

    commit many things ..

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,11 @@
-# flask_henry/app/main/__init__.py
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+
+@main.app_context_processor
+def inject_permissions():
+	return dict(Permission=Permission)
